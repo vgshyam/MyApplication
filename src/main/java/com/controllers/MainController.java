ef49b56b19c04f4ca71c3c4da18ab5e4bdd6de89
vgshyam <vgshyam.18@gmail.com>
2018-06-27 21:35:20 +0530

    First Commite

--- src/main/java/com/controllers/MainController.java
+++ src/main/java/com/controllers/MainController.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.domains.Student;
 import com.services.Services;
 
 @Controller
--- src/main/java/com/services/Services.java
+++ src/main/java/com/services/Services.java
@@ -1,11 +1,7 @@
 package com.services;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.domains.Student;
 import com.repositories.StudentRepository;
 
